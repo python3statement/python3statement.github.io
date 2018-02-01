5d012d05ab77ddf847639b31252edbc97f8a818c
Joseph Hamman <jhamman@ucar.edu>
2018-02-01 10:07:57 -0800

    no timeline

--- site.js
+++ site.js
@@ -178,10 +178,6 @@ $(document).ready(function (){
         {content: '0.18.x', start: '2016-10-16', end: '2017-10-16', py2:true},
         {content: '1.0.x -', start: '2016-12-26', end: '2021-12-23'}
     ],
-    'xarray':[
-        {content: '0.x Series', start: '2014-05-11', end: '2018-12-31', py2:true},
-        {content: '2018.1.0', start: '2019-01-01', end: '2022-12-31'}
-    ],
     // for tests, rando example
     //'matplotlib':[
     //  {content: 'matplotlib 2.x', start: '2015-06-01', end:'2018-06-01', py2:true},
