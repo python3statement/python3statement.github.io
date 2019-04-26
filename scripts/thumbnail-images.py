fe001e29691a781f1ffac194570a04b5c63bf328
Hugo <hugovk@users.noreply.github.com>
2019-04-26 13:39:43 +0300

    Use 320 x 160px for higher res screens

--- scripts/thumbnail-images.py
+++ scripts/thumbnail-images.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 """
-Thumbnail images to a maximum of 160px wide and 80px high
+Thumbnail images to a maximum of 320px wide and 160px high
 """
 import glob
 
 from PIL import Image  # pip install pillow
 
-max_size = 160, 80
+max_size = 320, 160
 
 # Exclude these images from thumbnailing
 excludes = ["assets/signac.png"]
