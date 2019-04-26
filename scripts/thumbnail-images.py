a7d85e501ffd5d0d3c188e0393d1593d21960d91
Hugo <hugovk@users.noreply.github.com>
2019-04-26 11:31:16 +0300

    Skip whitelisted images, and those already less than 160x80px

--- scripts/thumbnail-images.py
+++ scripts/thumbnail-images.py
@@ -9,7 +9,14 @@ from PIL import Image  # pip install pillow
 
 max_size = 160, 80
 
+# Exclude these images from thumbnailing
+excludes = ["assets/signac.png"]
+
 for infile in glob.glob("assets/*.png"):
+    if infile in excludes:
+        continue
     im = Image.open(infile)
+    if im.width <= max_size[0] and im.height <= max_size[1]:
+        continue
     im.thumbnail(max_size)
     im.save(infile)
