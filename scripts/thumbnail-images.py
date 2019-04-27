64d338b6995440e43ea27c5ba4cdc9220501efcb
Hugo <hugovk@users.noreply.github.com>
2019-04-27 13:47:46 +0300

    No need to exclude, it's now under 160px

--- scripts/thumbnail-images.py
+++ scripts/thumbnail-images.py
@@ -9,12 +9,7 @@ from PIL import Image  # pip install pillow
 
 max_size = 320, 160
 
-# Exclude these images from thumbnailing
-excludes = ["assets/signac.png"]
-
 for infile in glob.glob("assets/*.png"):
-    if infile in excludes:
-        continue
     im = Image.open(infile)
     if im.width <= max_size[0] and im.height <= max_size[1]:
         continue
