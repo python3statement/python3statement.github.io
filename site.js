13c791cd023a1f9e0530db810acabe88c53eb541
Masahiko Isshiki <masahiko_isshiki@yahoo.co.jp>
2019-06-24 18:33:28 +0900

    Fix site.js json

--- site.js
+++ site.js
@@ -308,11 +308,11 @@ $(document).ready(function (){
       {content: '1.x', start: '2015-12-10', end: '2018-02-14', py2:true},
       {content: '2.x', start: '2018-02-14', end: '2019-12-31', py2:true},
       {content: '3.x', start: '2019-11-05', end: '2023-12-31', py2:false},
-    ]
+    ],
     'Dask': [
       {content: '1', start: '2014-12-21', end: '2019-07-01', py2:true},
       {content: '2', start: '2019-11-30', end: '2024-11-30', py2:false},
-    ],
+    ]
 
     // for tests, rando example
     //'matplotlib':[
