b3036aa1868b693a371891c9e6e8e08418dc077b
Sébastien Tourbier <sebastien.tourbier1@gmail.com>
2020-09-04 14:51:18 +0200

    Add timeline of Connectome Mapper 3

--- site.js
+++ site.js
@@ -353,7 +353,11 @@ $(document).ready(function (){
     ],
     'lib3to6': [
       { content:'Python 3 only', start: '2018-09-01', end: '2023-12-31', py2:false},
-    ]
+    ],
+    'Connectome Mapper 3':[
+      {content: 'v3.0.0-beta-x beta series (Py2 only)', start: '2019-04-24', end:'2020-08-24', py2:true},
+      {content: 'v3.x.y[-RCz] releases and release candidates (Py3 only)', start: '2020-08-24', end:'2023-12-31'},
+    ],
 
     // for tests, rando example
     //'matplotlib':[
